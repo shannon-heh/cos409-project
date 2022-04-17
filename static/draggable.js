7eb651e60a5bcb8ee03201a0a01a7280a8553b3d
shannon-heh <shannon.heh@princeton.edu>
2022-04-16 23:05:00 -0400

    set up basic draggable elements with interactjs

---
+++ static/draggable.js
@@ -0,0 +1,111 @@
+import interact from "https://cdn.interactjs.io/v1.10.11/interactjs/index.js";
+
+interact(".draggable").draggable({
+  // enable inertial throwing
+  inertia: true,
+  // keep the element within this rectangle
+  modifiers: [
+    interact.modifiers.restrictRect({
+      restriction: "#workshop-container",
+      endOnly: true,
+    }),
+  ],
+  // enable autoScroll
+  autoScroll: true,
+
+  listeners: {
+    // call this function on every dragmove event
+    move: dragMoveListener,
+
+    // call this function on every dragend event
+    end(event) {
+      var textEl = event.target.querySelector("p");
+
+      textEl &&
+        (textEl.textContent =
+          "moved a distance of " +
+          Math.sqrt(
+            (Math.pow(event.pageX - event.x0, 2) +
+              Math.pow(event.pageY - event.y0, 2)) |
+              0
+          ).toFixed(2) +
+          "px");
+    },
+  },
+});
+
+function dragMoveListener(event) {
+  var target = event.target;
+  // keep the dragged position in the data-x/data-y attributes
+  var x = (parseFloat(target.getAttribute("data-x")) || 0) + event.dx;
+  var y = (parseFloat(target.getAttribute("data-y")) || 0) + event.dy;
+
+  // translate the element
+  target.style.transform = "translate(" + x + "px, " + y + "px)";
+
+  // update the posiion attributes
+  target.setAttribute("data-x", x);
+  target.setAttribute("data-y", y);
+}
+
+// this function is used later in the resizing and gesture demos
+window.dragMoveListener = dragMoveListener;
+
+// enable draggables to be dropped into this
+interact(".dropzone").dropzone({
+  // only accept elements matching this CSS selector
+  accept: ".item-drop",
+  // Require a 75% element overlap for a drop to be possible
+  overlap: 0.75,
+
+  // listen for drop related events:
+
+  ondropactivate: function (event) {
+    // add active dropzone feedback
+    console.log("drop activatee");
+    event.target.classList.add("drop-active");
+  },
+  ondragenter: function (event) {
+    console.log("drag enter");
+    event.preventDefault();
+
+    var draggableElement = event.relatedTarget;
+    var dropzoneElement = event.target;
+
+    // feedback the possibility of a drop
+    dropzoneElement.classList.add("drop-target");
+    draggableElement.classList.add("can-drop");
+    // draggableElement.textContent = "Dragged in";
+  },
+  ondragleave: function (event) {
+    console.log("drag leave");
+    // remove the drop feedback style
+    event.target.classList.remove("drop-target");
+    event.relatedTarget.classList.remove("can-drop");
+    // event.relatedTarget.textContent = "Dragged out";
+  },
+  ondrop: function (event) {
+    console.log("drop");
+    // event.relatedTarget.textContent = "Dropped";
+  },
+  ondropdeactivate: function (event) {
+    console.log("drop deactivate");
+
+    // remove active dropzone feedback
+    event.target.classList.remove("drop-active");
+    event.target.classList.remove("drop-target");
+  },
+});
+
+interact(".drag-drop").draggable({
+  inertia: true,
+  modifiers: [
+    interact.modifiers.restrictRect({
+      restriction: "#workshop-container",
+      endOnly: true,
+    }),
+  ],
+  autoScroll: true,
+  // dragMoveListener from the dragging demo above
+  listeners: { move: dragMoveListener },
+});
