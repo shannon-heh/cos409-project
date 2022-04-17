974f7c70a769d4df4cdb4d34d81fc5955076e6b7
shannon-heh <63625700+shannon-heh@users.noreply.github.com>
2022-04-17 13:51:01 -0400

    Add drag & drop + image renderings to workshop (#5)
    * fix workshop image syntax
    
    * set up basic draggable elements with interactjs
    
    * set up workshop background
    
    * add item on drop in, remove on drop out
    
    * support selecting material from pop-up
    
    - selecting on category opens pop-up to select material and show description
    - change cart from list to object
    - add image renderings for each category
    
    * prevent open modal on drag
    
    * add some styling to pop-up, adjust plant img size
    
    * update readme
    
    * remove x button on pop-up

---
+++ static/draggable.js
@@ -0,0 +1,111 @@
+import interact from "https://cdn.interactjs.io/v1.10.11/interactjs/index.js";
+import { addToCart, removeFromCart, showCategory } from "./app.js";
+
+interact(".draggable").draggable({
+  // enable inertial throwing
+  inertia: true,
+  // keep the element within this rectangle
+  modifiers: [
+    interact.modifiers.restrictRect({
+      restriction: "#workshop-container",
+      endOnly: true,
+    }),
+  ],
+  // enable autoScroll
+  autoScroll: true,
+
+  listeners: {
+    // call this function on every dragmove event
+    move: dragMoveListener,
+
+    // call this function on every dragend event
+    end(event) {
+      var textEl = event.target.querySelector("p");
+
+      textEl &&
+        (textEl.textContent =
+          "moved a distance of " +
+          Math.sqrt(
+            (Math.pow(event.pageX - event.x0, 2) +
+              Math.pow(event.pageY - event.y0, 2)) |
+              0
+          ).toFixed(2) +
+          "px");
+    },
+  },
+});
+
+function dragMoveListener(event) {
+  var target = event.target;
+  // keep the dragged position in the data-x/data-y attributes
+  var x = (parseFloat(target.getAttribute("data-x")) || 0) + event.dx;
+  var y = (parseFloat(target.getAttribute("data-y")) || 0) + event.dy;
+
+  // translate the element
+  target.style.transform = "translate(" + x + "px, " + y + "px)";
+
+  // update the posiion attributes
+  target.setAttribute("data-x", x);
+  target.setAttribute("data-y", y);
+}
+
+// this function is used later in the resizing and gesture demos
+window.dragMoveListener = dragMoveListener;
+
+// enable draggables to be dropped into this
+interact(".dropzone").dropzone({
+  // only accept elements matching this CSS selector
+  accept: ".category-container.drag-drop",
+  // Require a 75% element overlap for a drop to be possible
+  overlap: 0.75,
+
+  // listen for drop related events:
+  ondropactivate: function (event) {
+    // do not trigger click event on drag
+    $(".category-container").addClass("noclick");
+    // add active dropzone feedback
+    event.target.classList.add("drop-active");
+  },
+  ondragenter: function (event) {
+    var draggableElement = event.relatedTarget;
+    var dropzoneElement = event.target;
+
+    // feedback the possibility of a drop
+    dropzoneElement.classList.add("drop-target");
+    draggableElement.classList.add("can-drop");
+  },
+  ondragleave: function (event) {
+    const category = event.relatedTarget.getAttribute("data-category");
+    removeFromCart(category);
+
+    // remove the drop feedback style
+    event.target.classList.remove("drop-target");
+    event.relatedTarget.classList.remove("can-drop");
+  },
+  ondrop: function (event) {
+    const category = event.relatedTarget.getAttribute("data-category");
+    const item = event.relatedTarget
+      .querySelector(".item-choice")
+      .innerHTML.trim();
+
+    addToCart(category, item);
+  },
+  ondropdeactivate: function (event) {
+    // remove active dropzone feedback
+    event.target.classList.remove("drop-active");
+    event.target.classList.remove("drop-target");
+  },
+});
+
+interact(".drag-drop").draggable({
+  inertia: true,
+  modifiers: [
+    interact.modifiers.restrictRect({
+      restriction: "#workshop-container",
+      endOnly: true,
+    }),
+  ],
+  autoScroll: true,
+  // dragMoveListener from the dragging demo above
+  listeners: { move: dragMoveListener },
+});
